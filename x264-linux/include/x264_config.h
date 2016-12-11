10de3a677b3ffa76316739fa788cc27b9e41f3e2
Felix <wintelx264@163.com>
2016-12-11 22:50:37 +0800

    add some libs

---
+++ x264-linux/include/x264_config.h
@@ -0,0 +1,6 @@
+#define X264_BIT_DEPTH     8
+#define X264_GPL           1
+#define X264_INTERLACED    1
+#define X264_CHROMA_FORMAT 0
+#define X264_VERSION "0.148.x.N2"
+#define X264_POINTVER "0.148.x"
